cbe88a7b32f12291a3296b057dd53f34120c885d
N1Alice <kc700370@gmail.com>
2023-10-30 13:01:56 +0900

    더하기 기능 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,16 +2,20 @@ package com.ll;
 
 public class Calc {
     public static  int run(String exp) {
-        if(exp.contains("-")){
-            return 0;
-        }
 
-        if(exp.contains("+")){
-            return 20;
-        }
+        if(exp.isBlank()) return 0;
 
-        else{
-            return -1;
-        }
+
+        if(exp.contains("-")) return 0;
+
+
+
+        String[] exps = exp.split(" \\+ ");
+
+            int num1 = Integer.parseInt(exps[0]);
+            int num2 = Integer.parseInt(exps[1]);
+
+            return num1 + num2;
+
     }
 }
